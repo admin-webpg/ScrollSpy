8f1d06e25d34ce137d2c65ea5449ab199b2e375b
Mohen Falahnejad <Rojaweb1@gmail.com>
2024-03-11 15:58:12 +0330

    refactor: - change the package name. - add vite-plugin-dts for generate types

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { resolve } from "path";
+import { resolve } from "path"
+import dts from "vite-plugin-dts"
 
 export default {
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
-      name: "@web-pg/ScrollSpy",
+      name: "@webpg/scroll-spy",
       fileName: "ScrollSpy",
+      formats: ['es', 'umd'],
     },
   },
-};
+}
